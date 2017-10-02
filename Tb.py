b793d3d8246bf0b6f30b6f8576214d99904f3ea1
Prakhya Malyala <malyalaprakhya@gmail.com>
2017-10-02 19:21:31 -0400

    Update Tb.py

--- Tb.py
+++ Tb.py
@@ -2,7 +2,9 @@ from textblob import TextBlob, Word
 import sys
 import random
 
-initialuserq = raw_input("Hi, do you want to here my summary of the main points of the book we are discussing today?)"
+potentialqs= ["What did you think of the main character?", "Do you really feel like they had to follow the arc they did?"]
+
+initialuserq = raw_input("Hi, do you want to here my summary of the main points of the book we are discussing today?")
 # virtual book club summarizes book
 # builds and trains on these summaries to provide opinions, discussions
 if initialuserq == "yes":
@@ -12,17 +14,26 @@ if initialuserq == "yes":
 	'''
 
 	blob = TextBlob(text)
-	#blob.tags           
 	
 	#uses textblob documentation
 			 
 	nouns = list()
 	for word, tag in blob.tags:
-	    if tag == 'NN':
-		nouns.append(word.lemmatize())
+		if tag == 'NN':
+			nouns.append(word.lemmatize())
 
 	print "This text is about..."
-	for item in random.sample(nouns, 5):
+	for item in random.sample(nouns, 6):
 		word = Word(item)
 		print word.pluralize()
+	
+	if blob.sentiment.polarity < 0.5:
+    		negative = ["I didn't like this! It was too sad.", "This book was very dark for me."]
+    		print(random.choice(negative))
+
+	else:
+    		print("I thought this was a good book!")
+		
+raw_input(random.choice(potentialqs))
+
 
