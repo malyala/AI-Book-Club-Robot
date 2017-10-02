adbae7b9a5a5b6feaf3779a302a8381e1bf4a3d2
Prakhya Malyala <malyalaprakhya@gmail.com>
2017-10-02 18:10:37 -0400

    Update Tb.py

--- Tb.py
+++ Tb.py
@@ -2,20 +2,25 @@ from textblob import TextBlob, Word
 import sys
 import random
 
-text = '''
-Hello this book is about many things. A girl named Eliza finds a talking squirrel in the forest where she loses her grandmother's apple. "Have you ever lost an apple before?", Eliza asks.
-'''
+initialuserq = raw_input("Hi, do you want to here my summary of the main points of the book we are discussing today?)"
+# virtual book club summarizes book
+# builds and trains on these summaries to provide opinions, discussions
+if initialuserq == "yes":
+	  
+	text = '''
+	Hello this book is about many things. A girl named Eliza finds a talking squirrel in the forest where she loses her grandmother's apple. "Have you ever lost an apple before?", Eliza asks.
+	'''
 
-blob = TextBlob(text)
-#blob.tags           
+	blob = TextBlob(text)
+	#blob.tags           
 
-nouns = list()
-for word, tag in blob.tags:
-    if tag == 'NN':
-        nouns.append(word.lemmatize())
+	nouns = list()
+	for word, tag in blob.tags:
+	    if tag == 'NN':
+		nouns.append(word.lemmatize())
 
-print "This text is about..."
-for item in random.sample(nouns, 5):
-	word = Word(item)
-	print word.pluralize()
+	print "This text is about..."
+	for item in random.sample(nouns, 5):
+		word = Word(item)
+		print word.pluralize()
 
